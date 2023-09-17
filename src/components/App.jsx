b3cce90fe46ce275b02550b80a8f53dca9f7fa8c
olekskhom <oleks.khom@gmail.com>
2023-09-17 21:21:25 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from "./Profile/Profile";
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+   
     </div>
   );
 };
