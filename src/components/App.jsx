63b65f5bb5a71d5923077316c21500bf3bd1108a
olekskhom <oleks.khom@gmail.com>
2023-09-19 14:07:46 +0300

    data-fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Statistics from './statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from '../data/user.json';
-import data from '../data/data.json';
+// import data from '../data/data.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
 import css from './App.module.css';
